f5a04444d155d06f890e3f2b792b6985f69da63b
Miguel Coquet <miguel@mcoquet.com>
2023-04-18 00:48:35 +0100

    base interface

---
+++ interfaces/BaseInterface.js
@@ -0,0 +1,26 @@
+class BaseInterface {
+  constructor(options = {}) {
+    this.options = options;
+  }
+
+  async init() {
+    // Optional async initialization logic
+  }
+
+  async send(message, options = {}) {
+    // Send a message to the interface
+    throw new Error('Not implemented');
+  }
+
+  async receive(options = {}) {
+    // Receive a message from the interface
+    throw new Error('Not implemented');
+  }
+
+  async close() {
+    // Close the interface and release resources
+    throw new Error('Not implemented');
+  }
+}
+
+module.exports = BaseInterface;
