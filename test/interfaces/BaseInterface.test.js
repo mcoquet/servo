1a5c6244c17aca41523aa28e20ea06a507c035e1
Miguel Coquet <miguel@mcoquet.com>
2023-04-18 00:48:47 +0100

    tests for baseinterface

---
+++ test/interfaces/BaseInterface.test.js
@@ -0,0 +1,35 @@
+const BaseInterface = require('../../interfaces/BaseInterface');
+
+describe('BaseInterface', () => {
+  let baseInterface;
+
+  beforeEach(() => {
+    baseInterface = new BaseInterface();
+  });
+
+  test('should instantiate with default options', () => {
+    expect(baseInterface.options).toEqual({});
+  });
+
+  test('should instantiate with custom options', () => {
+    const customOptions = { exampleOption: 'exampleValue' };
+    baseInterface = new BaseInterface(customOptions);
+    expect(baseInterface.options).toEqual(customOptions);
+  });
+
+  test('init should be a function', () => {
+    expect(typeof baseInterface.init).toBe('function');
+  });
+
+  test('send should throw a Not implemented error', async () => {
+    await expect(baseInterface.send('test message')).rejects.toThrow('Not implemented');
+  });
+
+  test('receive should throw a Not implemented error', async () => {
+    await expect(baseInterface.receive()).rejects.toThrow('Not implemented');
+  });
+
+  test('close should throw a Not implemented error', async () => {
+    await expect(baseInterface.close()).rejects.toThrow('Not implemented');
+  });
+});
